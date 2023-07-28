37e6d8de16d9ed1f12e02ea59068e3aa6944b80e
vaibhav <vaibhav.singh@speckyfox.com>
2023-07-28 18:52:23 +0530

    work on about section

--- speckyfox-event/src/components/About/About.jsx
+++ speckyfox-event/src/components/About/About.jsx
@@ -6,36 +6,44 @@ export const About = () => {
   return (
     <div>
       <div className="about-header">ABOUT SPEAKER</div>
-      <div className="speaker-photo">
-        <strong>Abhishek Aggarwal...</strong>
-        <img src={speaker} alt="speaker photo" />
-      </div>
-      <div className="about-body">
-        Meet Abhishek Aggarwal, Co-Founder at SpeckyFox, a dynamic and
-        accomplished professional with a wealth of experience in the IT
-        industry. With an impressive 11-year tenure, Abhishek has honed his
-        skills while collaborating with a diverse range of clients, spanning
-        from small and mid-size businesses to Fortune 500 companies. As a
-        versatile individual, Abhishek effortlessly wears multiple hats and has
-        proved himself in various roles throughout his career. With a penchant
-        for excellence, Abhishek has specialized in Performance Engineering,
-        leveraging his technical expertise to optimize systems and processes,
-        ensuring seamless and efficient operations for his clients. His ability
-        to lead and inspire has allowed him to manage delivery teams with
-        aplomb, always ensuring that projects are executed with precision and
-        within set timelines. Intriguingly, Abhishek has an innate talent for
-        navigating the intricacies of pre-sales, showcasing his prowess in
-        convincing clients of the value and efficacy of the services offered by
-        his company, SpeckyFox Technologies. His exceptional negotiation skills
-        further complement his role as he crafts mutually beneficial agreements
-        that foster long-term partnerships. Embracing new challenges with
-        enthusiasm, Abhishek recently shifted his focus to an exciting role as
-        Chief Marketing Officer (CMO) at SpeckyFox Technologies. In this
-        capacity, he spearheads efforts to promote and enhance the company's
-        online visibility, crafting innovative marketing strategies that drive
-        growth and market presence. By combining his technical acumen with a
-        flair for marketing, Abhishek is poised to propel SpeckyFox Technologies
-        to new heights.
+      <div className="top">
+        <div className="speaker-photo">
+          <div className="backshape"></div>
+
+          <img className="imgcircle" src={speaker} alt="speaker photo" />
+
+          <strong className="botmphoto">Abhishek Aggarwal...</strong>
+        </div>
+        <div className="about-body">
+          <p>
+            Meet Abhishek Aggarwal, Co-Founder at SpeckyFox, a dynamic and
+            accomplished professional with a wealth of experience in the IT
+            industry. With an impressive 11-year tenure, Abhishek has honed his
+            skills while collaborating with a diverse range of clients, spanning
+            from small and mid-size businesses to Fortune 500 companies. As a
+            versatile individual, Abhishek effortlessly wears multiple hats and
+            has proved himself in various roles throughout his career. With a
+            penchant for excellence, Abhishek has specialized in Performance
+            Engineering, leveraging his technical expertise to optimize systems
+            and processes, ensuring seamless and efficient operations for his
+            clients. His ability to lead and inspire has allowed him to manage
+            delivery teams with aplomb, always ensuring that projects are
+            executed with precision and within set timelines. Intriguingly,
+            Abhishek has an innate talent for navigating the intricacies of
+            pre-sales, showcasing his prowess in convincing clients of the value
+            and efficacy of the services offered by his company, SpeckyFox
+            Technologies. His exceptional negotiation skills further complement
+            his role as he crafts mutually beneficial agreements that foster
+            long-term partnerships. Embracing new challenges with enthusiasm,
+            Abhishek recently shifted his focus to an exciting role as Chief
+            Marketing Officer (CMO) at SpeckyFox Technologies. In this capacity,
+            he spearheads efforts to promote and enhance the company's online
+            visibility, crafting innovative marketing strategies that drive
+            growth and market presence. By combining his technical acumen with a
+            flair for marketing, Abhishek is poised to propel SpeckyFox
+            Technologies to new heights.
+          </p>
+        </div>
       </div>
     </div>
   );
